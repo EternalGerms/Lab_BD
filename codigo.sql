f417a80a69c4e5aac2f8efb36319acab7179cc6b
Matheus Esposto <eternalgermes@proton.me>
2023-06-08 18:45:13 +0000

    trying again

--- codigo.sql
+++ codigo.sql
@@ -1,8 +1,14 @@
-CREATE TABLE IF NOT EXISTS "Ambulatorio"("nroa" INT, "andar" NUMERIC(3)	NOT NULL, "capacidade" SMALLINT);
-CREATE TABLE IF NOT EXISTS "Medicos"("codm" INT, "nomem" VARCHAR(40) NOT NULL, "idade" SMALLINT NOT NULL, "especialidade" CHAR(20), "cpf" NUMERIC(11) NOT NULL, "cidade" VARCHAR(30), "nroa" INT);
-CREATE TABLE IF NOT EXISTS "Pacientes"("codp" INT, "nomep" VARCHAR(40), "idade" SMALLINT NOT NULL, "cpf" NUMERIC(11) NOT NULL, "cidade" CHAR(30), "doenca" VARCHAR(40) NOT NULL);
-CREATE TABLE IF NOT EXISTS "Funcionarios"("codf" INT, "nomef" VARCHAR(40), "idade" SMALLINT, "cidade" VARCHAR(30), "salario" NUMERIC(10), "cpf" NUMERIC(11) NOT NULL);
-CREATE TABLE IF NOT EXISTS "Consultas"("codm" INT, "codp" INT, "data" DATE, "hora" TIME);
+DROP TABLE IF EXISTS Medicos;
+DROP TABLE IF EXISTS Ambulatorio;
+DROP TABLE IF EXISTS Pacientes;
+DROP TABLE IF EXISTS Funcionarios;
+DROP TABLE IF EXISTS Consultas;
+
+CREATE TABLE IF NOT EXISTS "Ambulatorio" ("nroa" INT, "andar" NUMERIC(3)NOT NULL, "capacidade" SMALLINT);
+CREATE TABLE IF NOT EXISTS "Medicos" ("codm" INT, "nomem" VARCHAR(40)NOT NULL, "idade" SMALLINT NOT NULL, "especialidade" CHAR(20), "cpf" NUMERIC(11)NOT NULL, "cidade" VARCHAR(30), "nroa" INT);
+CREATE TABLE IF NOT EXISTS "Pacientes" ("codp" INT, "nomep" VARCHAR(40), "idade" SMALLINT NOT NULL, "cpf" NUMERIC(11)NOT NULL, "cidade" CHAR(30), "doenca" VARCHAR(40)NOT NULL);
+CREATE TABLE IF NOT EXISTS "Funcionarios" ("codf" INT, "nomef" VARCHAR(40), "idade" SMALLINT, "cidade" VARCHAR(30), "salario" NUMERIC(10), "cpf" NUMERIC(11)NOT NULL);
+CREATE TABLE IF NOT EXISTS "Consultas" ("codm" INT, "codp" INT, "data" DATE, "hora" TIME);
 
 DELETE FROM Ambulatorio;
 DELETE FROM Medicos;
@@ -10,73 +16,75 @@ DELETE FROM Pacientes;
 DELETE FROM Funcionarios;
 DELETE FROM Consultas;
 
-INSERT INTO Ambulatorio (nroa, andar, capacidade)
-VALUES
-(1, 1, 30),
+INSERT INTO Ambulatorio(nroa, andar, capacidade)
+VALUES(1, 1, 30),
 (2, 1, 50),
 (3, 2, 40),
 (4, 2, 25),
 (5, 2, 55);
 
-INSERT INTO Medicos (codm, nomem, idade, especialidade, cpf, cidade, nroa)
-VALUES
-(1, "Joao", 40, "ortopedia", 12345678901, "Florianopolis", 1),
+INSERT INTO Medicos(codm, nomem, idade, especialidade, cpf, cidade, nroa)
+VALUES(1, "Joao", 40, "ortopedia", 12345678901, "Florianopolis", 1),
 (2, "Maria", 42, "traumatologia", 12345678902, "Blumenau", 1),
 (3, "Pedro", 51, "pediatria", 12345678903, "Sao Jose", 2),
 (4, "Carlos", 28, "ortopedia", 12345678904, "Joinville", 2),
 (5, "Marcia", 33, "neurologia", 12345678905, "Biguacu", 3);
 
-INSERT INTO Pacientes (codp, nomep, idade, cpf, cidade, doenca)
-VALUES
-(1, "Ana", 20, 09876543210,"Florianopolis", "gripe"),
+INSERT INTO Pacientes(codp, nomep, idade, cpf, cidade, doenca)
+VALUES(1, "Ana", 20, 09876543210, "Florianopolis", "gripe"),
 (2, "Paulo", 24, 09876543211, "Palhoca", "fratura"),
 (3, "Lucia", 30, 09876543212, "Biguacu", "tendinite"),
 (4, "Carlos", 28, 09876543212, "Joinville", "sarampo");
 
-INSERT INTO Funcionarios (codf, nomef, idade, cidade, salario, cpf)
-VALUES
-(1, "Rita", 32, "Sao Jose", 1200, 74185296301),
+INSERT INTO Funcionarios(codf, nomef, idade, cidade, salario, cpf)
+VALUES(1, "Rita", 32, "Sao Jose", 1200, 74185296301),
 (2, "Maria", 55, "Palhoca", 1220, 74185296302),
 (3, "Caio", 45, "Florianopolis", 1100, 74185296303),
 (4, "Carlos", 44, "Florianopolis", 1200, 74185296304),
 (5, "Paula", 33, "Florianopolis", 2500, 74185296305);
 
-INSERT INTO Consultas (codm, codp, data, hora)
-VALUES
-(1, 1, 2006/06/12, "14:00"),
-(1, 4, 2006/06/13, "10:00"),
-(2, 1, 2006/06/13, "9:00"),
-(2, 2, 2006/06/13, "11:00"),
-(2, 3, 2006/06/14, "14:00"),
-(2, 4, 2006/06/14, "17:00"),
-(3, 1, 2006/06/19, "18:00"),
-(3, 3, 2006/06/12, "10:00"),
-(3, 4, 2006/06/19, "13:00"),
-(4, 4, 2006/06/20, "13:00"),
-(4, 4, 2006/06/22, "19:30");
+INSERT INTO Consultas(codm, codp, data, hora)
+VALUES(1, 1, 2006 / 06 / 12, "14:00"),
+(1, 4, 2006 / 06 / 13, "10:00"),
+(2, 1, 2006 / 06 / 13, "9:00"),
+(2, 2, 2006 / 06 / 13, "11:00"),
+(2, 3, 2006 / 06 / 14, "14:00"),
+(2, 4, 2006 / 06 / 14, "17:00"),
+(3, 1, 2006 / 06 / 19, "18:00"),
+(3, 3, 2006 / 06 / 12, "10:00"),
+(3, 4, 2006 / 06 / 19, "13:00"),
+(4, 4, 2006 / 06 / 20, "13:00"),
+(4, 4, 2006 / 06 / 22, "19:30");
 
-ALTER TABLE Ambulatorio ADD "especialidade" VARCHAR(50); 
+ALTER TABLE Ambulatorio ADD "especialidade" VARCHAR(50);
 ALTER TABLE Medicos ADD "crm" VARCHAR(20);
 ALTER TABLE Funcionarios ADD "DATA_CONTRATACAO" DATE;
-ALTER TABLE Medicos ADD "SOBRENOME" VARCHAR(50); 
+ALTER TABLE Medicos ADD "SOBRENOME" VARCHAR(50);
 
-ALTER TABLE Funcionarios ADD "SOBRENOME" VARCHAR(50); 
+ALTER TABLE Funcionarios ADD "SOBRENOME" VARCHAR(50);
 
-ALTER TABLE Pacientes ADD "SOBRENOME" VARCHAR(50);  
-ALTER TABLE Medicos ADD "RG" VARCHAR(20); 
+ALTER TABLE Pacientes ADD "SOBRENOME" VARCHAR(50);
+ALTER TABLE Medicos ADD "RG" VARCHAR(20);
 
-ALTER TABLE Funcionarios ADD "RG" VARCHAR(20); 
+ALTER TABLE Funcionarios ADD "RG" VARCHAR(20);
 
-ALTER TABLE Pacientes ADD "RG" VARCHAR(20); 
-ALTER TABLE Pacientes RENAME COLUMN "idade" TO "IDADE_PACIENTE"; 
-ALTER TABLE Funcionarios RENAME COLUMN "idade" TO "IDADE_FUNCIONARIO"; 
+ALTER TABLE Pacientes ADD "RG" VARCHAR(20);
+ALTER TABLE Pacientes RENAME COLUMN "idade" TO "IDADE_PACIENTE";
+ALTER TABLE Funcionarios RENAME COLUMN "idade" TO "IDADE_FUNCIONARIO";
 ALTER TABLE Medicos RENAME COLUMN "idade" TO "IDADE_MEDICO";
 
-UPDATE Pacientes SET cidade ="ILHOTA" WHERE nomep='Paulo';
-UPDATE Pacientes SET cidade ="ILHOTA" WHERE codp=2; 
-UPDATE Consultas SET DATA="2006-07-04",HORA="12:00" WHERE codm = 4 AND codp=1; 
-UPDATE Pacientes SET doenca = "cancer" WHERE nomep="Ana";
-UPDATE Consultas SET hora = "14:30" WHERE codm=3 AND codp=4;
+UPDATE Pacientes SET cidade = "ILHOTA" WHERE nomep = 'Paulo';
+UPDATE Pacientes SET cidade = "ILHOTA" WHERE codp = 2;
+UPDATE Consultas SET DATA = "2006-07-04",
+HORA = "12:00" WHERE codm = 4 AND codp = 1;
+UPDATE Pacientes SET doenca = "cancer" WHERE nomep = "Ana";
+UPDATE Consultas SET hora = "14:30" WHERE codm = 3 AND codp = 4;
 DELETE FROM Funcionarios WHERE "codf" = 4;
-UPDATE Consultas SET hora= NULL, DATA = NULL WHERE hora= "19:00"; 
-UPDATE Pacientes SET nomep=NULL,idade=NULL,cpf=NULL,doenca=NULL, cidade=NULL WHERE doenca ="cancer" OR idade=10;
+UPDATE Consultas SET hora = NULL,
+DATA = NULL WHERE hora = "19:00";
+UPDATE Pacientes SET nomep = NULL,
+idade = NULL,
+cpf = NULL,
+doenca = NULL,
+cidade = NULL WHERE doenca = "cancer" OR idade = 10;
+
